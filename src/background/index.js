0022afa8f2de74a131efefcce05d14362a7d1b5e
cafe4it <cafe4it@gmail.com>
2016-03-18 23:27:22 +0700

    init && test

---
+++ src/background/index.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+/*
+ const _AnalyticsCode = 'UA-74453743-1';
+ let service, tracker;
+
+ var importScript = (function (oHead) {
+ //window.analytics = analytics;
+ function loadError(oError) {
+ throw new URIError("The script " + oError.target.src + " is not accessible.");
+ }
+
+ return function (sSrc, fOnload) {
+ var oScript = document.createElement("script");
+ oScript.type = "text\/javascript";
+ oScript.onerror = loadError;
+ if (fOnload) {
+ oScript.onload = fOnload;
+ }
+ oHead.appendChild(oScript);
+ oScript.src = sSrc;
+ }
+
+ })(document.head || document.getElementsByTagName("head")[0]);
+
+ importScript(chrome.runtime.getURL('shared/google-analytics-bundle.js'), function () {
+ console.info('google analytics platform loaded...');
+ service = analytics.getService('instagram_easy_downloader');
+ tracker = service.getTracker(_AnalyticsCode);
+ tracker.sendAppView('App view');
+ });
+ */
+
+var filter = {
+    urls : ["<all_urls>"],
+    types : ['images']
+}
+
+function cb_detail(details){
+    console.log(details);
+}
+
+chrome.webRequest.onCompleted.addListener(cb_detail, filter);
